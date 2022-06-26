b238bf9b0cdff0e8536707361cf5e86b43e8d306
Pii <media.mahadi77@gmail.com>
2022-06-27 01:10:24 +0600

    URL and View learned

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MyPost(models.Model):
+    Title = models.CharField(max_length=30)
+    Content = models.TextField()
+    author = models.CharField(max_length=20)
+    def __str__(self):
+        return self.Title
